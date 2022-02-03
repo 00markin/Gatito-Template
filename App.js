77b468f4855beb2a529f5e13eed15548a2f77db2
00markin <mvmarcus@ucl.br>
2022-02-03 13:11:07 -0300

    Reactotron

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
 import TelaPadrao from './src/componentes/TelaPadrao';
 import Rotas from './src/Rotas';
+import Reactotron from 'reactotron-react-native';
+
+Reactotron.configure().useReactNative().connect();
+console.tron = Reactotron;
 
 export default function App() {
+  console.tron.log('App iniciado');
   return (
     <TelaPadrao>
       <Rotas />
